cdf47cbe8fbfe5659d2b114327c4a0fe61e59330
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2022-12-26 21:51:09 -0500

    Detecting datasource

--- src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
+++ src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import javax.sql.DataSource;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SpringEcommerceApplication {
 
     public static void main(String[] args) {
