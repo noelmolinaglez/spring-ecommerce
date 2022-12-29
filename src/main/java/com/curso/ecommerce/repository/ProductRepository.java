472ec51564d2e2b56befd37e83fec015e3619d2f
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2022-12-28 21:36:38 -0500

    ADD ProductRepository.

---
+++ src/main/java/com/curso/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.curso.ecommerce.repository;
+
+import com.curso.ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
